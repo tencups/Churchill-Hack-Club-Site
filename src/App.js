57c67f66a6bae77baac176a93a871d8fc8bc2cae
tencups <26286782+tencups@users.noreply.github.com>
2019-08-22 13:39:46 -0400

    wtf nothing is working

--- src/App.js
+++ src/App.js
@@ -7,42 +7,44 @@ import Faq from "./components/Faq"
 import About from "./components/About"
 import "./App.css"
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <Card />
-      <About />
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Card />
+        <About />
 
-      <Faq
-        heading={true}
-        title="When and where are the meetings?"
-        content="We have meetings every Thursday at lunch in Mrs. Smith's room which is near the band room. Meetings will begin at 11:15 and end until the first bell rings. "
-      />
-      <Faq
-        title="What are meetings like?"
-        content="Depending on the week, we will be doing a range of activites from workshops, demos, hackathons and hack challenges where you can possibly win some money 😏! Members can also work together
+        <Faq
+          heading={true}
+          title="When and where are the meetings?"
+          content="We have meetings every Thursday at lunch in Mrs. Smith's room which is near the band room. Meetings will begin at 11:15 and end until the first bell rings. "
+        />
+        <Faq
+          title="What are meetings like?"
+          content="Depending on the week, we will be doing a range of activites from workshops, demos, hackathons and hack challenges where you can possibly win some money 😏! Members can also work together
         to form projects which they will be able to demo!"
-      />
-      <Faq
-        title="What topics do the workshops cover?"
-        content="Our workshops covers a range of topics from cybersecurity, web design, database management, Raspberry Pi computing, and many, many more!  "
-      />
-      <Faq
-        title="Does it cost money to join?"
-        content="Nope, everything is free! 🤩"
-      />
-      <Faq
-        title="What will we be coding on?"
-        content="You can either bring your own laptop or use an online IDE such as Repl.it. 💻"
-      />
+        />
+        <Faq
+          title="What topics do the workshops cover?"
+          content="Our workshops covers a range of topics from cybersecurity, web design, database management, Raspberry Pi computing, and many, many more!  "
+        />
+        <Faq
+          title="Does it cost money to join?"
+          content="Nope, everything is free! 🤩"
+        />
+        <Faq
+          title="What will we be coding on?"
+          content="You can either bring your own laptop or use an online IDE such as Repl.it. 💻"
+        />
 
-      <Faq title="Will there be food?" content="Maybe...  🤔" />
+        <Faq title="Will there be food?" content="Maybe...  🤔" />
 
-      <Contact />
-      <Footer />
-    </React.Fragment>
-  )
+        <Contact />
+        <Footer />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,80 @@
 import React from "react"
 import hackclublogo from "../hackclublogo.png"
-import styles from "./nav.module.css"
+import "./nav.css"
 import { Link } from "react-scroll"
 
-const Navbar = () => {
-  return (
-    <>
-      <header>
-        <img
-          src={hackclublogo}
-          width="216"
-          height="81"
-          className={styles.logo}
-        />
-        <nav>
-          <ul className={styles.nav_links}>
-            <li>
-              <Link
-                style={{ cursor: "pointer" }}
-                activeClass="active"
-                spy={true}
-                smooth={true}
-                to="about"
-                activeClassName="selected"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                style={{ cursor: "pointer" }}
-                activeClass="active"
-                spy={true}
-                smooth={true}
-                to="FAQ"
-                activeClassName="selected"
-              >
-                FAQ
-              </Link>
-            </li>
-            <li>
-              <Link
-                style={{ cursor: "pointer" }}
-                activeClass="active"
-                spy={true}
-                smooth={true}
-                to="contact"
-                activeClassName="selected"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  )
+class Navbar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      display: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        display: !prevState.display
+      }
+    })
+  }
+  render() {
+    return (
+      <>
+        <header>
+          <img src={hackclublogo} width="216" height="81" className="logo" />
+          <nav>
+            <div
+              className={this.state.display ? "open" : ""}
+              onClick={this.handleClick}
+              className="hamburger"
+            >
+              <div className="line" />
+              <div className="line" />
+              <div className="line" />
+            </div>
+            <ul className="nav_links">
+              <li>
+                <Link
+                  style={{ cursor: "pointer" }}
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  to="about"
+                  activeClassName="selected"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={{ cursor: "pointer" }}
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  to="FAQ"
+                  activeClassName="selected"
+                >
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={{ cursor: "pointer" }}
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  to="contact"
+                  activeClassName="selected"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </>
+    )
+  }
 }
 export default Navbar
